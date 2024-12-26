f3ceefc5a9110463b840e9f7bc0b4dddfdfa83ce
tomasz kowalczyk <tomasz.kowalczyk1983@gmail.com>
2024-12-26 12:18:11 +0000

    updated semantic versioning dependencies

--- release.config.js
+++ release.config.js
@@ -7,7 +7,7 @@ module.exports = {
         [
             '@semantic-release/git',
             {
-                assets: ['CHANGELOG.md', 'package.json'],
+                assets: ['CHANGELOG.md', 'composer.json'],
                 message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
             },
         ],
