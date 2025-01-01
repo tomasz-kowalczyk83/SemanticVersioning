ae14fda5ad72391043ceb064cc03f6932ed78a08
tomasz kowalczyk <tomasz.kowalczyk1983@gmail.com>
2025-01-01 19:57:47 +0000

    chore(cicd): add support for versioned branches

--- release.config.js
+++ release.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-    branches: ['main'],
+    branches: [
+        { name: "1.x", range: "1.x", channel: "1.x" },
+        { name: "2.x", range: "2.x", channel: "2.x" },
+        { name: "3.x", range: "3.x", channel: "3.x" },
+        { name: "main" },
+        { name: "next", channel: "next" },
+    ],
     plugins: [
         '@semantic-release/commit-analyzer', // Analyze commits to determine version
         '@semantic-release/release-notes-generator', // Generate release notes
